481f2d8f0539d04edefea2d1bf180014d047300c
Jathin <sjathinchowdhary@gmail.com>
2026-06-28 07:52:43 +0530

    Allow Root Endpoints

--- ghostchat/src/main/java/com/ghostchat/controller/HomeController.java
+++ ghostchat/src/main/java/com/ghostchat/controller/HomeController.java
@@ -8,6 +8,6 @@ public class HomeController {
 
     @GetMapping("/")
     public String home(){
-        return "GhostChat backend is running";
+        return "GhostChat backend is Running";
     }
 }
